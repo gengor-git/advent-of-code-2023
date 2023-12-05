5d74307d3e4e01f3c1e3d0b9de19a7774dc0a00e
Martin Palmowski <martin.palmowski@gmail.com>
2023-12-05 17:03:09 +0100

    Still only reading.

--- day05/seeding.py
+++ day05/seeding.py
@@ -13,32 +13,46 @@ def execute_seed_mapping(data_file) -> int:
     print("Seeds: {}".format(seeds))
 
 
+    # 'seed', 'soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity'
+
     n = 1
+    all_numbers = []
+    category = "none"
     while n < len(data):
         if data[n].startswith("seed"):
             print("seed mapping")
+            category = "seed"
         elif data[n].startswith("soil"):
             print("soil mapping")
+            category = "soil"
         elif data[n].startswith("fertilizer"):
             print("fertilizer mapping")
+            category = "fertilizer"
         elif data[n].startswith("water"):
             print("water mapping")
+            category = "water"
         elif data[n].startswith("light"):
             print("light mapping")
+            category = "light"
         elif data[n].startswith("temperature"):
             print("temperature mapping")
+            category = "temperature"
         elif data[n].startswith("humidity"):
             print("humidity mapping")
+            category = "humidity"
         elif data[n] != "":
             values = re.findall(r'\d+', data[n])
             if len(values) != 3:
                 raise ValueError("Too few values in mapping. Parsing error??")
-            print(values)
+            # print(values)
+            # [all_numbers.append(x) for x in values]
+            # save mapping values to a valid data construct
         n += 1
 
     return result
 
 
+
 if __name__ == "__main__":
     # execute_seed_mapping(sample_file)
-    execute_seed_mapping(input_file)
+    print(execute_seed_mapping(input_file))
