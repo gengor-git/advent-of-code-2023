b09cf0d96fae4f3662866b1fabb83d5a9088e68a
Martin Palmowski <martin.palmowski@gmail.com>
2023-12-06 11:01:09 +0100

    Skeleton to add stuff to a map.

--- day05/seeding.py
+++ day05/seeding.py
@@ -4,6 +4,16 @@ input_file = "day05/input.txt"
 sample_file = "day05/sample.txt"
 
 
+def add_to_map(source:int, dest:int, length:int, datamap:set) -> set:
+
+    i = 0
+    while i < length:
+        datamap[source+i] = dest+i
+        i += 1
+
+    return datamap
+
+
 def execute_seed_mapping(data_file) -> int:
     result = 0
     data = open(data_file).read().strip().splitlines()
@@ -18,37 +28,44 @@ def execute_seed_mapping(data_file) -> int:
     n = 1
     all_numbers = []
     category = "none"
+    mapping_seed_to_soil = {}
+    mapping_soil_to_fertilizer = {}
+    mapping_fertilizer_to_water = {}
+    mapping_water_to_light = {}
+    mapping_light_to_temperature = {}
+    mapping_temperature_to_humidity = {}
+    mapping_humidity_to_location = {}
     while n < len(data):
         if data[n].startswith("seed"):
-            print("seed mapping")
-            category = "seed"
+            print("Seed mapping")
+
+            n += 1
+            while data[n] != "":
+                print(data[n])
+                print(".")
+                values = re.findall(r'\d+', data[n])
+                if len(values) != 3:
+                    raise ValueError("Too few values in mapping. Parsing error??")
+                mapping_seed_to_soil = add_to_map(int(values[1]), int( values[0] ), int( values[2] ), mapping_seed_to_soil)
+                print(".")
+                n += 1
+
         elif data[n].startswith("soil"):
             print("soil mapping")
-            category = "soil"
         elif data[n].startswith("fertilizer"):
             print("fertilizer mapping")
-            category = "fertilizer"
         elif data[n].startswith("water"):
             print("water mapping")
-            category = "water"
         elif data[n].startswith("light"):
             print("light mapping")
-            category = "light"
         elif data[n].startswith("temperature"):
             print("temperature mapping")
-            category = "temperature"
         elif data[n].startswith("humidity"):
             print("humidity mapping")
-            category = "humidity"
-        elif data[n] != "":
-            values = re.findall(r'\d+', data[n])
-            if len(values) != 3:
-                raise ValueError("Too few values in mapping. Parsing error??")
-            # print(values)
-            # [all_numbers.append(x) for x in values]
-            # save mapping values to a valid data construct
+        else:
+            pass
         n += 1
-
+        print("Seed to soil: {}".format(mapping_seed_to_soil))
     return result
 
 
