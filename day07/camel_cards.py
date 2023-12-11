286582223d486f97a7ff418032e592610b16c6a4
Martin Palmowski <martin.palmowski@gmail.com>
2023-12-11 14:50:51 +0100

    Schnipp, schnapp code added.

--- day07/camel_cards.py
+++ day07/camel_cards.py
@@ -30,12 +30,14 @@ def calculate_winnings(data_file) -> int:
 
     cards = "AKQJT98765432"
 
+    hands = {}
     for play in data:
-        hand = hand_to_list(play.split()[0].strip())
+        hand = play.split()[0].strip()
+        hand_list = hand_to_list(hand)
         bid = int(play.split()[1].strip())
-        print("Hand {} with bid {}".format(hand, bid))
+        # print("Hand {} with bid {}".format(hand, bid))
 
-        c = Counter(hand)
+        c = Counter(hand_list)
         # Alternatives to counter:
         # (1)
         # hand_values = pd.Series(list(hand))
@@ -46,8 +48,18 @@ def calculate_winnings(data_file) -> int:
         #         hand_value[card_to_int(card)] = hand.count(card)
         # sorted_hand_value = dict(sorted(hand_value.items(), key=lambda item: item[1], reverse=True))
         # print(sorted_hand_value)
-        print(c)
-        print(c.most_common(1)[0][1])
+        # print(c)
+        # print(c.most_common(1)[0][1])
+        hands[hand] = (c, bid)
+    # for key in hands:
+    #     print("---")
+    #     print("Key most:   {}".format(key))
+    #     print("Amount:     {}".format(hands[key][0].most_common(1)[0][1]))
+    #     print("Card:       {}".format(hands[key][0].most_common(1)[0][0]))
+    #     # print(hands[key][1])
+    #     print("Key second: {}".format(key))
+    #     print("Amount:     {}".format(hands[key][0].most_common(2)[1][1]))
+    #     print("Card:       {}".format(hands[key][0].most_common(2)[1][0]))
 
     return result
 
